da838282798089f9e5623b1dd6256de08963d3bc
Athan Clark <athan.clark@gmail.com>
2023-11-15 19:08:08 -0500

    color picker integration

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SQUARE = SIDE * SIDE;
 
 
 window.onload = function() {
-    let lastColor = 'white';
+    let lastColor = document.getElementById('picker').value;
     let clicking = false;
 
     document.addEventListener('mousedown', function(e) {
@@ -23,18 +23,13 @@ window.onload = function() {
         block.style.height = `${100 / SIDE}%`;
         block.addEventListener('mousemove', function() {
             if (clicking) {
-                this.className = lastColor;
+                this.style.background = lastColor;
             }
         });
         canvas.appendChild(block);
     }
 
-    for (const color of document.querySelectorAll('#palette > div')) {
-        color.addEventListener('click', function() {
-            lastColor = this.className;
-            const indicator = document.getElementById('indicator');
-            indicator.className = lastColor;
-            indicator.textContent = lastColor;
-        });
-    }
+    document.getElementById('picker').addEventListener('change', function(e) {
+        lastColor = e.target.value;
+    });
 }
