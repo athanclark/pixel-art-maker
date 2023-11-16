9beef86f39ebc09635f32a0dc777788dbff75a39
Athan Clark <athan.clark@gmail.com>
2023-11-16 11:16:50 -0500

    local storage

--- app.js
+++ app.js
@@ -13,23 +13,41 @@ window.onload = function() {
     document.addEventListener('mouseup', function(e) {
         clicking = false;
         e.preventDefault();
+        let blocks = [];
+        for (const block of document.querySelectorAll('#canvas > div')) {
+            blocks.push(block.style.background);
+        }
+        window.localStorage.setItem('canvas', JSON.stringify(blocks));
     });
 
     const canvas = document.getElementById('canvas');
+    const prevBlocks = JSON.parse(window.localStorage.getItem('canvas'));
 
-    for (let i = 0; i < SQUARE; i++) {
+    const createBlock = function createBlock(bg) {
         const block = document.createElement('div');
         block.style.width = `${100 / SIDE}%`;
         block.style.height = `${100 / SIDE}%`;
+        block.style.background = bg;
         block.addEventListener('mousemove', function() {
             if (clicking) {
                 this.style.background = lastColor;
             }
         });
         canvas.appendChild(block);
+    };
+
+    if (prevBlocks) {
+        for (const blockBg of prevBlocks) {
+            createBlock(blockBg);
+        }
+    } else {
+        for (let i = 0; i < SQUARE; i++) {
+            createBlock(lastColor);
+        }
     }
 
     document.getElementById('picker').addEventListener('change', function(e) {
         lastColor = e.target.value;
     });
 }
+
